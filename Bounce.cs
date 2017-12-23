a67ca63ab6af99f1658a39e2fbd8237fed8ca06d
TROYSC <34628736+TROYSC@users.noreply.github.com>
2017-12-23 23:42:11 +0000

    Add files via upload

---
+++ Bounce.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour
+{
+    public float speed = 0.0005f;
+
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+}
