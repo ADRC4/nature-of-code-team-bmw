a9d2efd2cfd59d1f23d465fb023749a672c3b1fb
uczlgzo <34650116+uczlgzo@users.noreply.github.com>
2018-01-08 20:26:03 +0000

    Update MouseTracking.cs

--- Assets/06_06_PathFollowing_Assets/MouseTracking.cs
+++ Assets/06_06_PathFollowing_Assets/MouseTracking.cs
@@ -9,82 +9,82 @@ public class MouseTracking : MonoBehaviour
     static float up = 4.0f;
     static float down = -4.0f;
 
-    //两部车子的设置
+    //position of two vehicles
     static float[] maxforce = { 0.002f, 0.012f }; // Maximum steering force
     static float[] maxspeed = { 0.04f, 0.12f };    // Maximum speed
     Vector3[] location = { new Vector3(left, 0, 0), new Vector3(left, 0, 0) };
     Vector3[] velocity = { new Vector3(maxspeed[0], 0), new Vector3(maxspeed[1], 0) };
     Vector3[] acceleration = { new Vector3(), new Vector3() };
 
-    bool debug = true;//调试
+    bool debug = true;
 
-    List<Vector3> path = new List<Vector3>();//管道，4个点
+    List<Vector3> path = new List<Vector3>();//4points od path
 
     // Use this for initialization
     void Start()
     {
-        path.Add(new Vector3(left, 0));//左边中点
-        path.Add(new Vector3(Random.value * left, Random.value * 2.0f - 1f));//上下波动范围-1~1
-        path.Add(new Vector3(Random.value * right, Random.value * 2.0f - 1f));//上下波动范围-1~1
-        path.Add(new Vector3(right, 0));//右边中点
-        //assets里的edge是用来给三角形和圆形画外轮廓的，但是实践中发现不行，暂且留着，以后改
+        path.Add(new Vector3(left, 0));
+        path.Add(new Vector3(Random.value * left, Random.value * 2.0f - 1f));//Fluctuation range
+        path.Add(new Vector3(Random.value * right, Random.value * 2.0f - 1f));//Fluctuation range
+        path.Add(new Vector3(right, 0));
+        
 
-        //两个模版，要隐藏掉
+
         GameObject.Find("Circle").GetComponent<Renderer>().enabled = false;
         GameObject.Find("Triangle").GetComponent<Renderer>().enabled = false;
 
         {
             for (int i = 0; i < 2; i++)
             {
-                //克隆一堆东西，有圆和三角形
-                var tri = Instantiate(GameObject.Find("Triangle"));//小车本身
+               
+                var tri = Instantiate(GameObject.Find("Triangle"));//vehicle
                 tri.GetComponent<Renderer>().enabled = true;
                 tri.name = "Triangle" + i;
-                var cir = Instantiate(GameObject.Find("Circle"));//小车在轨道上的点
+                var cir = Instantiate(GameObject.Find("Circle"));
                 cir.GetComponent<Renderer>().enabled = true;
                 cir.name = "Circle" + i;
                 cir.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                var cir2 = Instantiate(GameObject.Find("Circle"));//小车前方的点
+                var cir2 = Instantiate(GameObject.Find("Circle"));
                 cir2.GetComponent<Renderer>().enabled = true;
                 cir2.name = "CircleT" + i;
                 cir2.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                var bling = Instantiate(GameObject.Find("Circle"));//小车在轨道上的较大的点，会闪
+                var bling = Instantiate(GameObject.Find("Circle"));//blingpoint
                 bling.GetComponent<Renderer>().enabled = true;
                 bling.name = "Bling" + i;
                 bling.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
 
 
                 {
-                    var borderObj = new GameObject("Trace" + i);//画小车前方的直线
+                    var borderObj = new GameObject("Trace" + i);
                     borderObj.name = "Trace" + i;
                     borderObj.transform.position = new Vector3(0, 0, -2.4f);
-                    var border = borderObj.AddComponent<LineRenderer>();//画线用
-                    border.material = new Material(Shader.Find("Sprites/Default"));//改成这样的就可以了
+                    var border = borderObj.AddComponent<LineRenderer>();
+                    border.material = new Material(Shader.Find("Sprites/Default"));
                     border.startColor = Color.black;
-                    border.endColor = Color.black; //设置直线颜色
+                    border.endColor = Color.black; 
                     border.startWidth = 0.02f;
-                    border.endWidth = 0.02f;//设置直线宽度
+                    border.endWidth = 0.02f;
                     border.positionCount = 3;
                     border.useWorldSpace = true;
-                    border.SetPosition(0, Vector3.zero);//把三个点要弄齐了
+                    border.SetPosition(0, Vector3.zero);
                     border.SetPosition(1, Vector3.zero);
                     border.SetPosition(2, Vector3.zero);
                 }
             }
         }
 
-        //画轨道
+       
         {
-            //轨道上的中心线，细
-            var borderObj = new GameObject("Line");//准备直线
+            
+            var borderObj = new GameObject("Line");
             borderObj.name = "Line";
             borderObj.transform.position = new Vector3(0, 0, -3f);
-            var border = borderObj.AddComponent<LineRenderer>();//画线用
-            border.material = new Material(Shader.Find("Sprites/Default"));//试来试去这个才是对的！！
-            border.startColor = Color.black;//这里线不是黑色的，要再研究下
-            border.endColor = Color.black; //设置直线颜色
+            var border = borderObj.AddComponent<LineRenderer>();
+            border.material = new Material(Shader.Find("Sprites/Default"));
+            border.startColor = Color.black;
+            border.endColor = Color.black; 
             border.startWidth = 0.02f;
-            border.endWidth = 0.02f;//设置直线宽度
+            border.endWidth = 0.02f;
             border.positionCount = 4;
             border.useWorldSpace = false;
             border.SetPosition(0, path[0]);
@@ -93,16 +93,16 @@ public class MouseTracking : MonoBehaviour
             border.SetPosition(3, path[3]);
         }
         {
-            //轨道背景，粗
+            //road 
             var borderObj = new GameObject("Line2");
             borderObj.name = "Line2";
             borderObj.transform.position = new Vector3(0, 0, -1f);
-            var border = borderObj.AddComponent<LineRenderer>();//画线用
-            border.material = new Material(Shader.Find("Sprites/Default"));//改成这样的就可以了
+            var border = borderObj.AddComponent<LineRenderer>();
+            border.material = new Material(Shader.Find("Sprites/Default"));
             border.startColor = Color.grey;
-            border.endColor = Color.grey; //设置直线颜色
+            border.endColor = Color.grey; 
             border.startWidth = 1f;
-            border.endWidth = 1f;//设置直线宽度
+            border.endWidth = 1f;
             border.positionCount = 4;
             border.useWorldSpace = false;
             border.SetPosition(0, path[0]);
@@ -115,7 +115,7 @@ public class MouseTracking : MonoBehaviour
 
     // A function to get the normal point from a point (p) to a line segment (a-b)
     // This function could be optimized to make fewer new Vector objects
-    //求向量p到线段ab上的交点
+    //the intersection between the line and vector p
     Vector3 GetNormalPoint(Vector3 p, Vector3 a, Vector3 b)
     {
         // Vector from a to p
@@ -134,7 +134,7 @@ public class MouseTracking : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            debug = !debug;//是否要隐藏掉
+            debug = !debug;
             for (var i = 0; i < 2; i++)
             {
                 var tri = GameObject.Find("Trace" + i);
@@ -149,35 +149,35 @@ public class MouseTracking : MonoBehaviour
         }
 
 
-        for (var k = 0; k < 2; k++)//对两辆小车操作
+        for (var k = 0; k < 2; k++)
         {
             // Predict location 25 (arbitrary choice) frames ahead
-            var predict = velocity[k];//小车速度向量
+            var predict = velocity[k];
             predict.Normalize();
             var pred = predict;
-            predict *= 0.8f;//调整速度，向前看
-            var predictLoc = location[k] + predict;//过一会小车要达到的位置
+            predict *= 0.8f;
+            var predictLoc = location[k] + predict;//predicted location
 
             // Now we must find the normal to the path from the predicted location
             // We look at the normal for each line segment and pick out the closest one
 
-            Vector3 normal = new Vector3();//小车的predict点到轨道上的法向量
-            Vector3 target = new Vector3();//小车的目标移动位置
+            Vector3 normal = new Vector3();
+            Vector3 target = new Vector3();
             float worldRecord = 1000000;  // Start with a very high record distance that can easily be beaten
 
             // Loop through all points of the path
             for (int i = 0; i < path.Count - 1; i++)
             {
                 // Look at a line segment
-                //线段
+              
                 var a = path[i];
                 var b = path[i + 1];
 
                 // Get the normal point to that line
-                var normalPoint = GetNormalPoint(predictLoc, a, b);//predictLoc点在ab线段上的垂线
+                var normalPoint = GetNormalPoint(predictLoc, a, b);//predictLoc's vertical line on the ab line
                 // This only works because we know our path goes from left to right
                 // We could have a more sophisticated test to tell if the point is in the line segment or not
-                //交点不在ab线段上，就默认为b点
+                //IF intersection NOT IN THE LINE AB ,THEN B POINT            
                 if (normalPoint.x < a.x || normalPoint.x > b.x)
                 {
                     // This is something of a hacky solution, but if it's not within the line segment
@@ -189,7 +189,7 @@ public class MouseTracking : MonoBehaviour
                 float distance = Vector3.Distance(predictLoc, normalPoint);//算一下从向前看的点到轨道上法向交点的距离
 
                 // Did we beat the record and find the closest line segment?
-                if (distance < worldRecord)//用于找最短距离，可能与另一轨道更近呢？
+                if (distance < worldRecord)//SHORTEST DISTANCE
                 {
                     worldRecord = distance;
                     // If so the target we want to steer towards is the normal
@@ -202,11 +202,10 @@ public class MouseTracking : MonoBehaviour
                     // Should be based on distance to path & velocity
                     dir *= 2f;
                     target = normalPoint;
-                    target += dir;//算出target，也就是小车直线要去的地方
+                    target += dir;//CALCULATE THE TARGET
                 }
             }
 
-            //一些debug图形的位置设置
             GameObject.Find("Circle" + k).transform.position = new Vector3(normal.x, normal.y, -2.5f);
             var bli = normal + pred * 0.4f;
             GameObject.Find("Bling" + k).transform.position = new Vector3(bli.x, bli.y, -2.5f);
@@ -220,7 +219,7 @@ public class MouseTracking : MonoBehaviour
 
             GameObject.Find("Triangle" + k).transform.position = new Vector3(location[k].x, location[k].y, -2.0f);//三角位置
             float rot = Vector3.Angle(velocity[k], Vector3.up);//旋转角度
-            //注意，velocity在第一、四象限时，角度计算不是以顺时针算的，是算最小角度，因此要2pi-rot
+           
             GameObject.Find("Triangle" + k).transform.rotation = Quaternion.AngleAxis(velocity[k].x < 0 ? rot : (2 * Mathf.PI - rot), Vector3.forward);//计算三角形的旋转角度
             //Debug.Log(velocity  +" "+Vector3.Angle(velocity, Vector3.right));
 
@@ -228,20 +227,20 @@ public class MouseTracking : MonoBehaviour
 
             // Only if the distance is greater than the path's radius do we bother to steer
             var sp = GameObject.Find("Bling" + k).GetComponent<SpriteRenderer>();
-            if (worldRecord > 0.5f)//什么时候要调整小车的方向？就是小车到轨道外面去的时候，由于轨道粗1.0f，所以这里是一半
+            if (worldRecord > 0.5f)
             {
                 Seek(k, target);
             }
 
-            sp.color = worldRecord > 0.45f ? Color.red : Color.black;//是否是红色
+            sp.color = worldRecord > 0.45f ? Color.red : Color.black;
 
-            velocity[k] += acceleration[k];//计算速度
-            velocity[k] = Vector3.ClampMagnitude(velocity[k], maxspeed[k]);//设置上限
+            velocity[k] += acceleration[k];//CALCULATE THE SPEED
+            velocity[k] = Vector3.ClampMagnitude(velocity[k], maxspeed[k]);//set the top
             location[k] += velocity[k];
             // Reset accelerationelertion to 0 each cycle
             acceleration[k] *= 0;
 
-            //边界重置
+            //reset the border
             if (predictLoc.x < left) location[k] = new Vector3(right, 0, location[k].z);
             //if (location.y < -r) location.y = height+r;
             if (predictLoc.x > right) location[k] = new Vector3(left, 0, location[k].z);
@@ -250,19 +249,19 @@ public class MouseTracking : MonoBehaviour
 
     // A method that calculates and applies a steering force towards a target
     // STEER = DESIRED MINUS VELOCITY
-    void Seek(int k, Vector3 target)//小车要调整方向了！
+    void Seek(int k, Vector3 target)
     {
         var desired = target - location[k];  // A vector pointing from the location to the target
 
         // If the magnitude of desired equals 0, skip out of here
         // (We could optimize this to check if x and y are 0 to avoid mag() square root
-        if (desired.magnitude < float.Epsilon) return;//都到了目标地点，不用走了
+        if (desired.magnitude < float.Epsilon) return;//arrive the target stop moving
 
         // Normalize desired and scale to maximum speed
         desired.Normalize();
         desired *= maxspeed[k];
         // Steering = Desired minus Velocity
-        Vector3 steer = desired - velocity[k];//老样子，拐过来
+        Vector3 steer = desired - velocity[k];
         steer = Vector3.ClampMagnitude(steer, maxforce[k]);  // Limit to maximum steering force
         acceleration[k] += steer;
     }
@@ -276,8 +275,8 @@ public class MouseTracking : MonoBehaviour
 
         //画轨道
         {
-            var borderObj = GameObject.Find("Line");//准备直线
-            var border = borderObj.GetComponent<LineRenderer>();//画线用
+            var borderObj = GameObject.Find("Line");
+            var border = borderObj.GetComponent<LineRenderer>();
             border.SetPosition(0, path[0]);
             border.SetPosition(1, path[1]);
             border.SetPosition(2, path[2]);
@@ -285,7 +284,7 @@ public class MouseTracking : MonoBehaviour
         }
         {
             var borderObj = GameObject.Find("Line2");
-            var border = borderObj.GetComponent<LineRenderer>();//画线用
+            var border = borderObj.GetComponent<LineRenderer>();
             border.SetPosition(0, path[0]);
             border.SetPosition(1, path[1]);
             border.SetPosition(2, path[2]);
