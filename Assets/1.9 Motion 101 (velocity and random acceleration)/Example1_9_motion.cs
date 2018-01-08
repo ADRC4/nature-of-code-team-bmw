a59b4ef4064afd1aa09490ca1613f6ac2c9d990b
isacclian <34666210+isacclian@users.noreply.github.com>
2018-01-08 21:28:25 +0000

    Add files via upload

---
+++ Assets/1.9 Motion 101 (velocity and random acceleration)/Example1_9_motion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Example1_9;
+using Assets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Example1_9_motion : MonoBehaviour {
+
+    Mover mover;
+
+    void Start()
+    {
+        Helper.size(200, 200);
+
+        Camera.main.transform.Translate(new Vector3(0, 0, 20.0f));
+        Camera.main.transform.Rotate(Vector3.right * 180.0f);
+
+        mover = new Mover();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnDrawGizmos()
+    {
+        if (mover == null)
+            return;
+
+        Helper.background(255);
+
+        mover.update();
+        mover.checkEdges();
+        mover.display();
+    }
+
+
+   
+}
