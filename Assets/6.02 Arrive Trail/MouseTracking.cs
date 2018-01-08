a700547f5375a03ec2d3c74bc000e6019677f7d2
uczlgzo <34650116+uczlgzo@users.noreply.github.com>
2018-01-08 20:49:46 +0000

    Update MouseTracking.cs

--- Assets/6.02 Arrive Trail/MouseTracking.cs
+++ Assets/6.02 Arrive Trail/MouseTracking.cs
@@ -18,13 +18,13 @@ public class MouseTracking : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        //assets里的edge是用来给三角形和圆形画外轮廓的，但是实践中发现不行，暂且留着，以后改
+       
 
         GameObject.Find("Circle").GetComponent<Renderer>().enabled = false;
         GameObject.Find("Triangle").GetComponent<Renderer>().enabled = false;
         lineObject = new GameObject("Line");
         lineObject.name = "Line";
-        lineObject.AddComponent<LineRenderer>();//画线用
+        lineObject.AddComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -34,63 +34,63 @@ public class MouseTracking : MonoBehaviour {
             return;
 
         // Update velocity
-        velocity += acceleration;//计算速度
-        velocity = Vector3.ClampMagnitude(velocity, maxspeed);//设置上限
+        velocity += acceleration;
+        velocity = Vector3.ClampMagnitude(velocity, maxspeed);
         location += velocity;
         // Reset accelerationelertion to 0 each cycle
         acceleration *= 0;
 
-        history.Add(new Vector3(location.x, location.y, -5.0f));//增加轨迹节点，-5是z坐标，越大越在底层
+        history.Add(new Vector3(location.x, location.y, -5.0f));
         if (history.Count > 100)
         {
-            history.RemoveAt(0);//去掉最老的线
+            history.RemoveAt(0);//remove the earliest line
         }
 
         if (history.Count > 1)
         {
             var line = lineObject.GetComponent<LineRenderer>();
-            //只有设置了材质 setColor才有作用
+            
             line.material = new Material(Shader.Find("Particles/Additive"));
-            line.positionCount = history.Count;//设置两点
-            line.startColor = Color.black;//这里线不是黑色的，要再研究下
-            line.endColor = Color.grey; //设置直线颜色
+            line.positionCount = history.Count;
+            line.startColor = Color.black;
+            line.endColor = Color.grey; //trail_color
             line.startWidth = 0.01f;
-            line.endWidth = 0.01f;//设置直线宽度
+            line.endWidth = 0.01f;//trail_width
 
-            //设置指示线的起点和终点
+           
             for (int i = 0; i < history.Count; i++)
             {
-                line.SetPosition(i, history[i]);//轨迹节点
+                line.SetPosition(i, history[i]);//Track nodes
             }
         }
-        GameObject.Find("Circle").transform.position = new Vector3(mouse.x, mouse.y, -1.0f);//圆位置
-        GameObject.Find("Triangle").transform.position = new Vector3(location.x, location.y, -5.0f);//三角位置
-        float rot = Vector3.Angle(velocity, Vector3.up);//旋转角度
-        //注意，velocity在第一、四象限时，角度计算不是以顺时针算的，是算最小角度，因此要2pi-rot
+        GameObject.Find("Circle").transform.position = new Vector3(mouse.x, mouse.y, -1.0f);//position of circle
+        GameObject.Find("Triangle").transform.position = new Vector3(location.x, location.y, -5.0f);//position of vehicle
+        float rot = Vector3.Angle(velocity, Vector3.up);//rotated degree
+        
         GameObject.Find("Triangle").transform.rotation = Quaternion.AngleAxis(velocity.x < 0 ? rot : (2 * Mathf.PI - rot), Vector3.forward);//计算三角形的旋转角度
         //Debug.Log(velocity  +" "+Vector3.Angle(velocity, Vector3.right));
     }
 
     void OnMouseUp()
     {
-        running = false;//停止运动
+        running = false;
     }
 
-    // 鼠标拖动
+   
     void OnMouseDrag()
     {
         running = true;
-        if (!showCircle)//显示圆形
+        if (!showCircle)
         {
             showCircle = true;
             GameObject.Find("Circle").GetComponent<Renderer>().enabled = true;
-            GameObject.Find("Triangle").GetComponent<Renderer>().enabled = true;//显示出来
+            GameObject.Find("Triangle").GetComponent<Renderer>().enabled = true;
         }
 
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);//获取鼠标所在位置（屏幕坐标，范围0-你的分辨率）
-        //要进行转换ScreenToWorldPoint将屏幕坐标转换为世界坐标，世界坐标的范围是x和y在-10到10，z在0到-10(摄影机坐标)
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      
         mouse.z = 0;
-        //三角形到鼠标距离
+       
         Vector3 desired = mouse - location;  // A vector pointing from the location to the target
         float d = desired.magnitude;
         // Normalize desired and scale with arbitrary damping within 100 pixels
